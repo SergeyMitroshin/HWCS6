054dc5aac7848cfbfa37507b6df10e367c379c34
SergeyMitroshin <sergey@host-best.com>
2022-11-28 23:35:18 +0300

    v01

--- example41/Program.cs
+++ example41/Program.cs
@@ -5,4 +5,19 @@
 1, -7, 567, 89, 223-> 3
 */
 
-Console.WriteLine("Hello, World!");
+int GetValue (string message)
+{
+    Console.WriteLine (message);
+    return int.Parse(Console.ReadLine());
+}
+
+int GetPositiveCount()
+{
+    int result = 0;
+    int amount = GetValue("Введите количество чисел");
+    for (int i = 1; i <= amount; i++)
+        if (GetValue("Введите число "+i) > 0) result++;
+    return result;
+}
+
+Console.WriteLine("Результат: "+GetPositiveCount());
